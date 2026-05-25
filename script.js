5e8895dd3f483462cab08bd9a0b91375b8793dff
joaoroberto988 <joao.syroka@escola.pr.gov.br>
2026-05-25 11:53:57 +0000

    g

---
+++ script.js
@@ -0,0 +1,7 @@
+agrinho-2026/
+│
+├── index.html      (HTML completo)
+├── style.css       (CSS completo - veja resposta anterior)
+├── script.js       (JavaScript completo - este arquivo)
+│
+└── README.md       (opicional - documentação do projeto)
